f2249a5e7a2174e2842f27746912ecb67667610a
NathanaelGazzard <nathanaelgazzard@gmail.com>
2023-11-06 10:46:05 +1100

    Installed PHP

---
+++ games.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Games</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/home.css" />
+  <script src="js/home.js"></script>
+</head>
+
+<body>
+  <header>
+    <?php include 'header.php'; ?>
+  </header>
+
+  <main></main>
+
+  <footer>
+    <p>Email: nathanael.gazzard@gmail.com</p>
+    <p>Contact Number: 0472 602 363</p>
+  </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<h1>NATE</h1>
+<h2 class="subtitle">nathanael gazzard</h2>
+<div id="navbar">
+    <ul>
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="games.php">Games</a>
+        </li>
+        <li>
+            <a href="about.php">About</a>
+        </li>
+        <li>
+            <a href="contact.php">Contact</a>
+        </li>
+    </ul>
+</div>
